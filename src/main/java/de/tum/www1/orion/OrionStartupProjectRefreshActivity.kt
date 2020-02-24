8d104874a95fe19e1a1596389db89f0c60507f8d
Alexander Ungar <alexander.ungar@tum.de>
2020-02-24 17:56:34 +0100

    Refactor exercise methods into new serivce

--- src/main/java/de/tum/www1/orion/OrionStartupProjectRefreshActivity.kt
+++ src/main/java/de/tum/www1/orion/OrionStartupProjectRefreshActivity.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.startup.StartupActivity
 import com.intellij.testFramework.runInEdtAndGet
 import de.tum.www1.orion.connector.client.JavaScriptConnector
 import de.tum.www1.orion.connector.ide.vcs.submit.ChangeSubmissionContext
+import de.tum.www1.orion.exercise.OrionExerciseService
 import de.tum.www1.orion.exercise.registry.BrokenRegistryLinkException
 import de.tum.www1.orion.exercise.registry.OrionGlobalExerciseRegistryService
 import de.tum.www1.orion.exercise.registry.OrionProjectRegistryStateService
@@ -15,7 +16,6 @@ import de.tum.www1.orion.settings.OrionSettingsProvider
 import de.tum.www1.orion.ui.util.BrokenLinkWarning
 import de.tum.www1.orion.util.appService
 import de.tum.www1.orion.util.service
-import de.tum.www1.orion.vcs.OrionGitAdapter
 
 class OrionStartupProjectRefreshActivity : StartupActivity {
 
@@ -52,7 +52,7 @@ class OrionStartupProjectRefreshActivity : StartupActivity {
     private fun prepareExercise(registry: OrionStudentExerciseRegistry, project: Project) {
         registry.exerciseInfo?.let { exerciseInfo ->
             project.messageBus.syncPublisher(OrionIntellijStateNotifier.INTELLIJ_STATE_TOPIC).openedExercise(exerciseInfo.exerciseId, exerciseInfo.view)
-            OrionGitAdapter.updateExercise(project)
+            project.service(OrionExerciseService::class.java).updateExercise()
         }
     }
 }
