791772b00d397c0862a7a2dba5f28b6ea2f2b60d
Alexander Ungar <alexander.ungar@netlight.com>
2020-09-20 22:47:57 +0200

    Workaround for https://github.com/JetBrains/gradle-intellij-plugin/issues/537

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
+pluginManagement {
+    repositories {
+        maven{
+            setUrl("https://jetbrains.bintray.com/intellij-plugin-service")
+        }
+        maven {
+            setUrl("https://plugins.gradle.org/m2/")
+        }
+        mavenCentral()
+    }
+}
+
 rootProject.name = "orion"
 
