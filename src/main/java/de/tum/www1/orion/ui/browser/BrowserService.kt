8b2cc2f5ab6d82df787e1b015370d6b762cc5382
Alexander Ungar <alexander.ungar@netlight.com>
2020-10-27 00:08:38 +0100

    Correctly set user agent on page loading

--- src/main/java/de/tum/www1/orion/ui/browser/BrowserService.kt
+++ src/main/java/de/tum/www1/orion/ui/browser/BrowserService.kt
@@ -6,10 +6,7 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.ui.components.JBLabel
-import com.intellij.ui.jcef.JBCefApp
-import com.intellij.ui.jcef.JBCefBrowser
-import com.intellij.ui.jcef.JBCefClient
-import com.intellij.ui.jcef.JBCefJSQuery
+import com.intellij.ui.jcef.*
 import com.intellij.util.messages.Topic
 import de.tum.www1.orion.connector.ide.build.OrionBuildConnector
 import de.tum.www1.orion.connector.ide.exercise.OrionExerciseConnector
@@ -26,35 +23,38 @@ import org.cef.browser.CefFrame
 import org.cef.handler.CefLoadHandler
 import org.cef.handler.CefLoadHandlerAdapter
 import org.cef.handler.CefMessageRouterHandler
+import org.cef.handler.CefRequestHandlerAdapter
+import org.cef.network.CefRequest
 import java.util.*
 import javax.swing.JComponent
 
 /**
  * Initialize the browser and the associated settings, and provide the UI Component to add into the ToolWindow.
  */
-class BrowserService(val project: Project) : IBrowser, Disposable{
+class BrowserService(val project: Project) : IBrowser, Disposable {
     private val jbCefBrowser: JBCefBrowser
     private val jsQuery: JBCefJSQuery
     private val client: JBCefClient
 
     init {
         val version = ResourceBundle.getBundle("de.tum.www1.orion.Orion").getString("version")
-        val userAgent=ServiceManager.getService(OrionSettingsProvider::class.java).getSetting(OrionSettingsProvider.KEYS.USER_AGENT) + " Orion/" + version
-        val route = project.service<OrionRouter>().routeForCurrentExercise() ?: project.service<OrionRouter>().defaultRoute()
+        val userAgent = ServiceManager.getService(OrionSettingsProvider::class.java)
+            .getSetting(OrionSettingsProvider.KEYS.USER_AGENT) + " Orion/" + version
+        val route =
+            project.service<OrionRouter>().routeForCurrentExercise() ?: project.service<OrionRouter>().defaultRoute()
         //Since JBCef wrapper doesn't support setting user-agent, we need to use reflection to access private properties.
-        val jbCefAppInstance= JBCefApp.getInstance()
-        val privateCefApp=jbCefAppInstance.getPrivateProperty<CefApp>("myCefApp")
-        val privateCefSettings=privateCefApp.getPrivateProperty<CefSettings>("settings_")
+        val jbCefAppInstance = JBCefApp.getInstance()
+        val privateCefApp = jbCefAppInstance.getPrivateProperty<CefApp>("myCefApp")
+        val privateCefSettings = privateCefApp.getPrivateProperty<CefSettings>("settings_")
         //Reading the source code of JBCefSettings we see that resource_dir_path should be the base path for JCEF, from
         //there we can buiild a path of cef_cache. This may not be true for Mac
-        val jcefPath=privateCefSettings.resources_dir_path
+        val jcefPath = privateCefSettings.resources_dir_path
         //Setting cache_path is necessary for saving logins.
-        privateCefSettings.cache_path="$jcefPath/cache"
-        privateCefSettings.persist_session_cookies=true
-        //The user agent needes to set before the call to createClient and can not be changed after
-        privateCefSettings.user_agent=userAgent
+        privateCefSettings.cache_path = "$jcefPath/cache"
+        privateCefSettings.persist_session_cookies = true
         client = jbCefAppInstance.createClient()
         jbCefBrowser = JBCefBrowser(client, null)
+        setUserAgentHandlerFor(userAgent)
         client.addLoadHandler(object : CefLoadHandlerAdapter() {
             override fun onLoadEnd(browser: CefBrowser?, frame: CefFrame?, httpStatusCode: Int) {
                 if (browser == null)
@@ -68,6 +68,23 @@ class BrowserService(val project: Project) : IBrowser, Disposable{
         //loading happen, if it's too late, then the window.cefQuery object won't be injected by JCEF
         injectJSBridge()
         jbCefBrowser.loadURL(route) //We only load until now to make sure that all handlers are registered
+    }
+
+    private fun setUserAgentHandlerFor(userAgent: String) {
+        client.addLoadHandler(object : CefLoadHandlerAdapter() {
+            override fun onLoadStart(
+                browser: CefBrowser,
+                frame: CefFrame?,
+                transitionType: CefRequest.TransitionType?
+            ) {
+                browser.executeJavaScript("""
+                    Object.defineProperty(navigator, 'userAgent', {
+                        get: function () { return '${userAgent}'; }
+                    });
+                """.trimIndent(), browser.url, 0
+                )
+            }
+        }, jbCefBrowser.cefBrowser)
     }
 
     private fun injectJSBridge() {
@@ -86,7 +103,7 @@ class BrowserService(val project: Project) : IBrowser, Disposable{
     }
 
     override val uiComponent: JComponent
-        get(){
+        get() {
             if (!JBCefApp.isSupported()) {
                 return JBLabel("JCEF support is not found in this IDE version. Please update your IDE.")
             }
