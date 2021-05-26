2483c132735792f5084fb36b8f9160bf6cfc3481
martin <ga27kin@mytum.de>
2021-05-26 15:53:17 +0200

    refactor importing

--- src/main/java/de/tum/www1/orion/exercise/OrionExerciseService.kt
+++ src/main/java/de/tum/www1/orion/exercise/OrionExerciseService.kt
@@ -4,7 +4,6 @@ import com.intellij.ide.impl.ProjectUtil
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.application.runInEdt
-import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
@@ -20,35 +19,31 @@ import de.tum.www1.orion.messaging.OrionIntellijStateNotifier
 import de.tum.www1.orion.ui.util.ImportPathChooser
 import de.tum.www1.orion.ui.util.notify
 import de.tum.www1.orion.util.OrionProjectUtil.newEmptyProject
-import de.tum.www1.orion.util.appService
 import de.tum.www1.orion.vcs.OrionGitAdapter
 import de.tum.www1.orion.vcs.OrionGitAdapter.clone
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.IOException
-import java.nio.file.Paths
 
 class OrionExerciseService(private val project: Project) {
-    fun cloneAndOpenExercise(
+    private fun createProject(
         exercise: ProgrammingExercise,
         exerciseView: ExerciseView,
-        cloneFunction: (chosenPath: String, projectPath: String) -> Unit
+        cloneFunction: (chosenPath: String, globalRegistry: OrionGlobalExerciseRegistryService) -> Unit
     ) {
-        val registry = when (exerciseView) {
-            ExerciseView.INSTRUCTOR -> project.service<OrionInstructorExerciseRegistry>()
-            ExerciseView.STUDENT -> project.service<OrionStudentExerciseRegistry>()
-        }
-        if (!registry.alreadyImported(exercise.id, exerciseView)) {
+        val registry = service<OrionGlobalExerciseRegistryService>()
+        if (!registry.isImported(exercise.id, exerciseView)) {
             runInEdt(ModalityState.NON_MODAL) {
                 val chooser = ImportPathChooser(project, exercise, ExerciseView.STUDENT)
                 if (chooser.showAndGet()) {
-                    val path = chooser.chosenPath
-                    newEmptyProject(exercise.title, path)?.basePath?.let {
-                        cloneFunction.invoke(path, it)
-                    }
+                    FileUtil.ensureExists(File(chooser.chosenPath))
+                    cloneFunction.invoke(chooser.chosenPath, registry)
+                } else {
+                    project.messageBus.syncPublisher(OrionIntellijStateNotifier.INTELLIJ_STATE_TOPIC).isCloning(false)
                 }
             }
         } else {
+            project.messageBus.syncPublisher(OrionIntellijStateNotifier.INTELLIJ_STATE_TOPIC).isCloning(false)
             val exercisePath = project.service<OrionGlobalExerciseRegistryService>()
                 .getPathForImportedExercise(exercise.id, exerciseView)
             invokeLater { ProjectUtil.openOrImport(exercisePath, project, false) }
@@ -56,32 +51,40 @@ class OrionExerciseService(private val project: Project) {
     }
 
     fun editExercise(exercise: ProgrammingExercise) {
-        cloneAndOpenExercise(exercise, ExerciseView.INSTRUCTOR) { chosenPath, projectPath ->
-            clone(
-                project, exercise.templateParticipation.repositoryUrl.toString(),
-                projectPath, "$projectPath/exercise", null
-            )
-            clone(
-                project, exercise.testRepositoryUrl.toString(),
-                projectPath, "$projectPath/tests", null
-            )
-            clone(
-                project, exercise.solutionParticipation.repositoryUrl.toString(),
-                projectPath, "$projectPath/solution"
-            ) {
-                // After cloning all repos, create the necessary project files and notify the webview about the opened project
-                prepareProjectForImport(File(projectPath))
-                project.service<OrionInstructorExerciseRegistry>()
-                    .onNewExercise(exercise, ExerciseView.INSTRUCTOR, chosenPath)
-                ProjectUtil.openOrImport(projectPath, project, false)
+        createProject(exercise, ExerciseView.INSTRUCTOR) { chosenPath, registry ->
+            try {
+                // Create a new empty project
+                val projectPath = newEmptyProject(exercise.title, chosenPath)!!.basePath!!
+                // Clone all base repositories
+                clone(
+                    project, exercise.templateParticipation.repositoryUrl.toString(),
+                    projectPath, projectPath + "/exercise", null
+                )
+                clone(
+                    project, exercise.testRepositoryUrl.toString(),
+                    projectPath, projectPath + "/tests", null
+                )
+                clone(
+                    project, exercise.solutionParticipation.repositoryUrl.toString(),
+                    projectPath, projectPath + "/solution"
+                ) {
+                    // After cloning all repos, create the necessary project files and notify the webview about the opened project
+                    prepareProjectForImport(File(projectPath))
+                    registry.registerExercise(exercise, ExerciseView.INSTRUCTOR, chosenPath)
+                    ProjectUtil.openOrImport(projectPath, project, false)
+                }
+            } catch (e: IOException) {
+                LoggerFactory.getLogger(OrionExerciseConnector::class.java).error(e.message, e)
+                project.notify(e.toString())
             }
         }
     }
 
     fun importParticipation(repositoryUrl: String, exercise: ProgrammingExercise) {
-        cloneAndOpenExercise(exercise, ExerciseView.STUDENT) { chosenPath, projectPath ->
-            clone(project, repositoryUrl, projectPath, chosenPath) {
-                project.service<OrionStudentExerciseRegistry>().onNewExercise(exercise, ExerciseView.STUDENT, chosenPath)
+        createProject(exercise, ExerciseView.STUDENT) { chosenPath, registry ->
+            val parent = LocalFileSystem.getInstance().refreshAndFindFileByPath(chosenPath)!!.parent.path
+            clone(project, repositoryUrl, parent, chosenPath) {
+                registry.registerExercise(exercise, ExerciseView.STUDENT, chosenPath)
                 ProjectUtil.openOrImport(chosenPath, project, false)
             }
         }
