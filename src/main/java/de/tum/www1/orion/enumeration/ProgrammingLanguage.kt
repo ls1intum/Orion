f645ffb68271d32f4f8a2ee24af852147008442f
Martin Dunker <61357727+MDK5@users.noreply.github.com>
2021-08-16 17:45:04 +0200

    [Bugfix] Prevent Crashes From Unsupported Programming Languages (#62)

--- src/main/java/de/tum/www1/orion/enumeration/ProgrammingLanguage.kt
+++ src/main/java/de/tum/www1/orion/enumeration/ProgrammingLanguage.kt
@@ -2,7 +2,11 @@ package de.tum.www1.orion.enumeration
 
 /**
  * Defines the programming language used in the exercise
+ * Has to be synchronized with Artemis's ProgrammingLanguage enumeration
+ *
+ * Most values are unused, but still required, otherwise Orion will throw an exception
+ * when downloading an exercise in that language
  */
 enum class ProgrammingLanguage {
-    JAVA, PYTHON, C
+    JAVA, PYTHON, C, HASKELL, KOTLIN, VHDL, ASSEMBLER, SWIFT, OCAML, EMPTY
 }
