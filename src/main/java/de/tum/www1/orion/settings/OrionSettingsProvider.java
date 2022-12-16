e04f3f88d7e60f9f0d6f99a2339aebeeb681b8fc
Jan Philip Bernius <bernius@tum.de>
2022-12-16 11:42:43 +0100

    Update URLs to new cit.tum.de URLs.

--- src/main/java/de/tum/www1/orion/settings/OrionSettingsProvider.java
+++ src/main/java/de/tum/www1/orion/settings/OrionSettingsProvider.java
@@ -10,7 +10,7 @@ public interface OrionSettingsProvider {
     boolean isModified(Map<KEYS, String> settings);
 
     enum KEYS {
-        ARTEMIS_URL("de.tum.www1.orion.settings.artemis.url", "https://artemis.ase.in.tum.de"),
+        ARTEMIS_URL("de.tum.www1.orion.settings.artemis.url", "https://artemis.cit.tum.de"),
         PROJECT_BASE_DIR("de.tum.www1.orion.settings.projects.path", System.getProperty("user.home") + File.separatorChar + "ArtemisProjects"),
         INSTRUCTOR_BASE_DIR("de.tum.www1.orion.settings.projects.instructor.path", System.getProperty("user.home") + File.separatorChar + "ArtemisProjects" + File.separatorChar + "Instructor"),
         TUTOR_BASE_DIR("de.tum.www1.orion.settings.projects.tutor.path", System.getProperty("user.home") + File.separatorChar + "ArtemisProjects" + File.separatorChar + "Tutor"),
