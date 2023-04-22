1c0870cda680be6cebfacce0382516d7725b9d40
Kroko-fant <schrottsuperdeluxe@gmail.com>
2023-04-22 09:02:32 +0200

    Updated Dependencies and Gradle for Version 2022.3 (#84)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
-    kotlin("jvm") version "1.6.21"
-    id("org.jetbrains.intellij") version "1.9.0"
+    kotlin("jvm") version "1.8.10"
+    id("org.jetbrains.intellij") version "1.13.1"
 }
 
 java {
@@ -23,32 +23,29 @@ repositories {
 
 dependencies {
     // JSON parsing
-    implementation("com.google.code.gson:gson:2.9.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2022.2.2")
-    plugins.set(listOf("git4idea", "maven", "PythonCore:222.4167.29"))
+    pluginName.set("intellij-orion")
+    version.set("2023.1")
+    plugins.set(listOf("Git4Idea", "maven", "PythonCore:231.8109.144"))
 }
 
 tasks {
     patchPluginXml {
         // Last 2 digits of the year and the major version digit, 211-211.* equals (20)21.1.*
         // See https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html
-        sinceBuild.set("222")
+        sinceBuild.set("231")
         // Orion Plugin version. Needs to be incremented for every new release!
-        version.set("1.2.1")
+        version.set("1.2.2")
         changeNotes.set(
             """<p>
             <h1>Version Upgrade</h1>
             <h2>Improvements</h2>
             <ul>
-                <li>Add support for assessment of exams, complaints and more feedback requests</li>
-                <li>Add support structured grading criteria</li>
-                <li>Fix an issue with artemis urls with trailing slashes</li>
-                <li>Fix an issue with quotes in feedback comments</li>
                 <li>Update dependencies</li>
             </ul>
         </p>"""
