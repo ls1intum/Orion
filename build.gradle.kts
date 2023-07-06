943adc87b364598f0d0cf793c0ccf2019f6bf328
Kroko-fant <schrottsuperdeluxe@gmail.com>
2023-07-06 16:44:55 +0200

    small bugfix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-fun properties(key: String) = providers.gradleProperty(key)
+fun properties(key: String): Provider<String> {
+    return providers.gradleProperty(key)
+}
+
 fun environment(key: String) = providers.environmentVariable(key)
 
 // its sadly not possible to put these values in a properties file
 plugins {
     id("java")
     kotlin("jvm") version "1.8.10"
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.intellij") version "1.14.2"
 }
 
 java {
@@ -35,20 +38,17 @@ dependencies {
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
     pluginName.set(properties("pluginName").get())
-    if (!properties("platformType").orElse("").equals("")) {
-        type.set(properties("platformType").get())
-    }
+
     version.set(properties("platformVersion").get())
     plugins.set(listOf("Git4Idea", "maven", "PythonCore:231.8109.144"))
 }
 
 tasks {
     patchPluginXml {
-        // Last 2 digits of the year and the major version digit, 211-211.* equals (20)21.1.*
-        // See https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html
+
         sinceBuild.set(properties("pluginSinceBuild").get())
         // Orion Plugin version. Needs to be incremented for every new release!
-        version.set(version)
+        version.set(properties("pluginVersion").get())
         changeNotes.set(
             """<p>
             <h1>Removed Deprecation</h1>
