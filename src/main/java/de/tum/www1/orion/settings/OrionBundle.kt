659e51132769e5a80c50a0e2e96094998a714d6d
Kroko-fant <schrottsuperdeluxe@gmail.com>
2023-09-11 17:41:46 +0200

    added documentation

--- src/main/java/de/tum/www1/orion/settings/OrionBundle.kt
+++ src/main/java/de/tum/www1/orion/settings/OrionBundle.kt
@@ -7,9 +7,17 @@ import java.lang.ref.Reference
 import java.lang.ref.SoftReference
 import java.util.*
 
+/**
+ * A resource bundle to manage translations
+ */
 object OrionBundle {
     private var ourBundle: Reference<ResourceBundle>? = null
     private const val BUNDLE: @NonNls String = "i18n.OrionBundle"
+
+    /**
+     * Requests a key from the bundle.
+     * @param key  The unique key to request from a resource
+     */
     fun message(key: @PropertyKey(resourceBundle = BUNDLE) String, vararg params: Any?): String {
         return AbstractBundle.message(bundle!!, key, *params)
     }
